3647b2f7f11090f104771ed5677550931d99419c
harikiruthi <kiruthikakiruthika50@gmail.com>
2020-01-10 08:25:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow','pink','white','brown']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(180):
+    t.pencolor(colors[x%9])
+    t.width(x/100 + 1)
+    t.forward(x)
+    t.left(59)
